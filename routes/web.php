1171578c164ca7e76e7ce9f4ab6bff61222d82af
Thabiso Ngubane <Thabison@nect2014.local>
2021-10-22 16:47:22 +0200

    landing page with the reservation form and ccs

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'ReservationController@index')->name('home');
+Route::resource('reservation','ReservationController');
+Route::resource('room','RoomController');
